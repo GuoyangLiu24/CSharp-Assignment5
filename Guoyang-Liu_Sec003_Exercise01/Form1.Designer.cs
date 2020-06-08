fe3a785a0dc105b422c4007cc1aee682b66caf46
GuoyangLiu24 <liuguoyang24@gmail.com>
2020-06-08 00:19:38 -0400

    C# Assignment5 Backup

---
+++ Guoyang-Liu_Sec003_Exercise01/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Guoyang_Liu_Sec003_Exercise01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.factonalDisplayLabel = new System.Windows.Forms.Label();
+            this.factonalTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radomTextBox = new System.Windows.Forms.TextBox();
+            this.searchValueTextBox = new System.Windows.Forms.TextBox();
+            this.lowIndexTextBox = new System.Windows.Forms.TextBox();
+            this.highIndexTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CharRadioButton = new System.Windows.Forms.RadioButton();
+            this.DoubleRadioButton = new System.Windows.Forms.RadioButton();
+            this.IntegerRadioButton = new System.Windows.Forms.RadioButton();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.evenOrOddTextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.oddEvenLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.factonalDisplayLabel);
+            this.groupBox1.Controls.Add(this.factonalTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.calculateButton);
+            this.groupBox1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 211);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "(1)Calculate Asynchronously";
+            // 
+            // factonalDisplayLabel
+            // 
+            this.factonalDisplayLabel.AutoSize = true;
+            this.factonalDisplayLabel.Location = new System.Drawing.Point(9, 165);
+            this.factonalDisplayLabel.Name = "factonalDisplayLabel";
+            this.factonalDisplayLabel.Size = new System.Drawing.Size(0, 15);
+            this.factonalDisplayLabel.TabIndex = 9;
+            // 
+            // factonalTextBox
+            // 
+            this.factonalTextBox.Location = new System.Drawing.Point(141, 61);
+            this.factonalTextBox.Name = "factonalTextBox";
+            this.factonalTextBox.Size = new System.Drawing.Size(113, 25);
+            this.factonalTextBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Get Factonal of: ";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(9, 119);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radomTextBox);
+            this.groupBox3.Controls.Add(this.searchValueTextBox);
+            this.groupBox3.Controls.Add(this.lowIndexTextBox);
+            this.groupBox3.Controls.Add(this.highIndexTextBox);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.CharRadioButton);
+            this.groupBox3.Controls.Add(this.DoubleRadioButton);
+            this.groupBox3.Controls.Add(this.IntegerRadioButton);
+            this.groupBox3.Controls.Add(this.resultTextBox);
+            this.groupBox3.Controls.Add(this.generateButton);
+            this.groupBox3.Controls.Add(this.displayButton);
+            this.groupBox3.Controls.Add(this.searchButton);
+            this.groupBox3.Location = new System.Drawing.Point(295, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(493, 426);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "(3)Display List of Values and Search";
+            // 
+            // radomTextBox
+            // 
+            this.radomTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radomTextBox.Location = new System.Drawing.Point(6, 85);
+            this.radomTextBox.Multiline = true;
+            this.radomTextBox.Name = "radomTextBox";
+            this.radomTextBox.Size = new System.Drawing.Size(92, 325);
+            this.radomTextBox.TabIndex = 16;
+            // 
+            // searchValueTextBox
+            // 
+            this.searchValueTextBox.Location = new System.Drawing.Point(286, 117);
+            this.searchValueTextBox.Name = "searchValueTextBox";
+            this.searchValueTextBox.Size = new System.Drawing.Size(100, 25);
+            this.searchValueTextBox.TabIndex = 10;
+            // 
+            // lowIndexTextBox
+            // 
+            this.lowIndexTextBox.Location = new System.Drawing.Point(286, 207);
+            this.lowIndexTextBox.Name = "lowIndexTextBox";
+            this.lowIndexTextBox.Size = new System.Drawing.Size(100, 25);
+            this.lowIndexTextBox.TabIndex = 11;
+            // 
+            // highIndexTextBox
+            // 
+            this.highIndexTextBox.Location = new System.Drawing.Point(286, 275);
+            this.highIndexTextBox.Name = "highIndexTextBox";
+            this.highIndexTextBox.Size = new System.Drawing.Size(100, 25);
+            this.highIndexTextBox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Input Value for Search:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(155, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Input Low Index:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(147, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Input High Index:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(112, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(303, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Output of Values between Low and High";
+            // 
+            // CharRadioButton
+            // 
+            this.CharRadioButton.AutoSize = true;
+            this.CharRadioButton.Location = new System.Drawing.Point(208, 39);
+            this.CharRadioButton.Name = "CharRadioButton";
+            this.CharRadioButton.Size = new System.Drawing.Size(60, 19);
+            this.CharRadioButton.TabIndex = 8;
+            this.CharRadioButton.TabStop = true;
+            this.CharRadioButton.Text = "Char";
+            this.CharRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DoubleRadioButton
+            // 
+            this.DoubleRadioButton.AutoSize = true;
+            this.DoubleRadioButton.Location = new System.Drawing.Point(115, 39);
+            this.DoubleRadioButton.Name = "DoubleRadioButton";
+            this.DoubleRadioButton.Size = new System.Drawing.Size(84, 19);
+            this.DoubleRadioButton.TabIndex = 7;
+            this.DoubleRadioButton.TabStop = true;
+            this.DoubleRadioButton.Text = "Doubles";
+            this.DoubleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // IntegerRadioButton
+            // 
+            this.IntegerRadioButton.AutoSize = true;
+            this.IntegerRadioButton.Location = new System.Drawing.Point(6, 39);
+            this.IntegerRadioButton.Name = "IntegerRadioButton";
+            this.IntegerRadioButton.Size = new System.Drawing.Size(92, 19);
+            this.IntegerRadioButton.TabIndex = 6;
+            this.IntegerRadioButton.TabStop = true;
+            this.IntegerRadioButton.Text = "Integers";
+            this.IntegerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(115, 338);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(363, 72);
+            this.resultTextBox.TabIndex = 5;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(337, 37);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(141, 23);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "Generate Values";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(403, 249);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(75, 26);
+            this.displayButton.TabIndex = 3;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(403, 123);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.oddEvenLabel);
+            this.groupBox2.Controls.Add(this.evenOrOddTextBox);
+            this.groupBox2.Controls.Add(this.checkButton);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 229);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 209);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "(2)Check for Even/Odd";
+            // 
+            // evenOrOddTextBox
+            // 
+            this.evenOrOddTextBox.Location = new System.Drawing.Point(117, 65);
+            this.evenOrOddTextBox.Name = "evenOrOddTextBox";
+            this.evenOrOddTextBox.Size = new System.Drawing.Size(137, 25);
+            this.evenOrOddTextBox.TabIndex = 9;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(69, 149);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(185, 23);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "Check for Even or Odd";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Input Number:";
+            // 
+            // oddEvenLabel
+            // 
+            this.oddEvenLabel.AutoSize = true;
+            this.oddEvenLabel.Location = new System.Drawing.Point(3, 101);
+            this.oddEvenLabel.Name = "oddEvenLabel";
+            this.oddEvenLabel.Size = new System.Drawing.Size(55, 15);
+            this.oddEvenLabel.TabIndex = 10;
+            this.oddEvenLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox factonalTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox searchValueTextBox;
+        private System.Windows.Forms.TextBox lowIndexTextBox;
+        private System.Windows.Forms.TextBox highIndexTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton CharRadioButton;
+        private System.Windows.Forms.RadioButton DoubleRadioButton;
+        private System.Windows.Forms.RadioButton IntegerRadioButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox evenOrOddTextBox;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox radomTextBox;
+        private System.Windows.Forms.Label factonalDisplayLabel;
+        private System.Windows.Forms.Label oddEvenLabel;
+    }
+}
+
---
+++ Guoyang-Liu_Sec003_Exercise01/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+namespace Guoyang_Liu_Sec003_Exercise01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //groupBox 1
+        private async void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long number = int.Parse(factonalTextBox.Text);
+                long factorNumber = await myFactorFun2(number);
+                factonalDisplayLabel.Text = $"Factonal of {number} is:{factorNumber}";
+            }
+            
+            catch
+            {
+                MessageBox.Show("Please enter a valied number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        // displays factor numbers in factorTextBox
+        private async Task<long> myFactorFun2(long n)
+        {
+            // find primes less than maximum
+            
+                if (n == 0)
+                    return 1;
+                       
+                long temp = await myFactorFun2(n - 1) * n;
+                
+                return temp;                      
+        }//groupBox1 end
+
+        //groupBox2
+       
+        private async void checkButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number2 = int.Parse(evenOrOddTextBox.Text);
+                Func<int, bool> IsEven = (int number) => number % 2 == 0;
+                Func<int, bool> IsOdd = (int number) => number % 2 != 0;
+                if (IsEven(number2))
+                {
+                    //  evenOrOddResultLabel.Visible = true;
+                    oddEvenLabel.Text = $"{number2} is an even";
+                }
+                else if (IsOdd(number2))
+                {
+                    //evenOrOddResultLabel.Visible = true;
+                    oddEvenLabel.Text = $"{number2} is an odd";
+                }             
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid number!");
+            }
+            
+        }
+
+        //groupBox2 end
+
+        //groupBox3
+        int[] integerArray = new int[10];
+        double[] doubleArray = new double[10];
+        char[] charArray = new char[10];
+        Random random = new Random();
+        private async void generateButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            try
+            {
+                if (IntegerRadioButton.Checked)
+                {                  
+                    for (int i = 0; i < 10; i++)
+                    {
+
+                        integerArray[i] = random.Next(10, 99);
+                        radomTextBox.AppendText($"{integerArray[i]}{Environment.NewLine}");
+                    }
+                }
+                else if (DoubleRadioButton.Checked)
+                {
+                    string result = null;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        doubleArray[i] = Math.Round(rnd.NextDouble() * (100 - 10) + 10, 2);
+                        result = doubleArray[i].ToString();
+                        radomTextBox.AppendText(result + Environment.NewLine);
+                    }
+
+                }
+                else if (CharRadioButton.Checked)
+                {
+                    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                    chars.ToCharArray();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int index = random.Next(0, 51);
+                        char selectedChar = Convert.ToChar(chars.Substring(index, 1));
+                        radomTextBox.AppendText(selectedChar + Environment.NewLine);
+                        charArray[i] = selectedChar;
+                    }
+                }               
+            }
+            catch
+            {
+                MessageBox.Show("Please select your option!");
+            }
+        }
+
+        private bool SearhData<T>(List<T> list, T value) where T : IComparable<T>
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(value) == 0)
+                {
+                    return true;
+                    //break;
+                }
+            }
+
+            return false;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (IntegerRadioButton.Checked)
+            {
+                try
+                {
+                    int searchIntValue = int.Parse(searchValueTextBox.Text); //Convert.ToInt32(searchValueTextBox.Text.ToString());
+                    List<int> intList = new List<int>(integerArray);//. OfType<int>().ToList();
+                    if (SearhData(intList, searchIntValue))
+                        MessageBox.Show(searchIntValue + " " + " is Found");
+                    else
+                        MessageBox.Show(searchIntValue + " " + " is not found");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Wrong information! ", MessageBoxButtons.OK);
+                }
+
+            }
+            if (DoubleRadioButton.Checked)
+            {
+                try
+                {
+                    if (!searchValueTextBox.Text.ToString().Contains("."))
+                    {
+                        MessageBox.Show("Please enter a double value");
+                    }
+                    else
+                    {
+                        double searchDoubleValue = Convert.ToDouble(searchValueTextBox.Text.ToString());
+                        List<double> doubleList = new List<double>(doubleArray);
+                        if (SearhData(doubleList, searchDoubleValue))
+                            MessageBox.Show(searchDoubleValue + " " + "Found");
+                        else
+                            MessageBox.Show(searchDoubleValue + " " + "Not found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Wrong information! ", MessageBoxButtons.OK);
+                }
+            }
+            if (CharRadioButton.Checked)
+            {
+                try
+                {
+                    char searchCharValue = Convert.ToChar(searchValueTextBox.Text.ToString());
+                    List<char> charList = new List<char>(charArray);
+                    if (Char.IsDigit(searchCharValue))
+                    {
+                        MessageBox.Show("Please enter a character");
+                    }
+                    else if (SearhData(charList, searchCharValue))
+                    {
+                        MessageBox.Show(searchCharValue + " " + "Found");
+                    }
+                    else
+                    {
+                        MessageBox.Show(searchCharValue + " " + "Not found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Wrong information! ", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int lowIndex = Convert.ToInt32(lowIndexTextBox.Text);
+                int highIndex = Convert.ToInt32(highIndexTextBox.Text);
+                if (lowIndex < highIndex)
+                {
+                    string result = null;
+                    for (int i = lowIndex; i <= highIndex; i++)
+                    {
+                        if (IntegerRadioButton.Checked) result += integerArray[i-1] + ", ";
+                        if (DoubleRadioButton.Checked) result += doubleArray[i-1] + ", ";
+                        if (CharRadioButton.Checked) result += charArray[i-1] + ", ";
+                    }
+                    resultTextBox.Text = result;
+                }
+                else MessageBox.Show("Low index can not be greater than high index.");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
